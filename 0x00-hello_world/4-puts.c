eb7349c20a113444828305a9c4306f512d61fe14
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-06-16 10:59:29 -0700

    print strin using puts and return 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* main: use puts function for the output and return 0 */
+/**
+ *  main -  use puts function for the output and
+ *
+ *  return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
