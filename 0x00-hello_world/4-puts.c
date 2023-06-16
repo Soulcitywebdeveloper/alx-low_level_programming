1fe8e899b686f6df7fa8188338095dc6f2a6b22f
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-06-16 11:38:17 -0700

    puts output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  main: Entry point and only allowed output
+ *  main - Entry point and only allowed output
  *
- *  return: return zero value when succesfull
+ *  Return: Alway 0 (succesfull)
 */
 int main(void)
 {
