afce144d2012283652cb533f08934a1660e21041
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-06-17 14:32:01 -0700

    lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,16 @@
 */
 int main(void)
 {
-	char alphabet;
+	int alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
 		putchar(alphabet);
+	}
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
 		putchar(alphabet);
 		putchar('\n');
+	}
 	return (0);
 }
