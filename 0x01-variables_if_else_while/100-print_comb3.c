7b2de142c012842ec5bcbd9a7f980d6715ac553a
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-06-18 14:47:02 -0700

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - Entry point and only allowed output
+ *
+ *  Return: Alway 0 (succesfull)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 90)
+	{
+		if (n % 10 > n / 10)
+		{
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+			if (n < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+	}
+	return (0);
+}
