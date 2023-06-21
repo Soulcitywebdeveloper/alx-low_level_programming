61d8b052bdad6e7bd591d46a0e4c2c800b05b312
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-06-21 12:51:46 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - Entry point and only allowed output
+ *
+ *  Return: Alway 0 (succesfull)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
