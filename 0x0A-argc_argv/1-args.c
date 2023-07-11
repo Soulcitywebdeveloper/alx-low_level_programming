03052984ea8c96f1099a501e2a09e73534e4972b
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-07-11 16:17:45 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
