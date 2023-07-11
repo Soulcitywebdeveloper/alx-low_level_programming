f567bb85112071c20a0b34689cd11c15d8096ee1
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-07-11 15:43:18 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
