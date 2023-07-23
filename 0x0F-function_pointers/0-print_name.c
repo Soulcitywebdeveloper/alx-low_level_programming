95bf06c921501aa17d8610cf28e083476f2cde14
Soulcitywebdeveloper <97274245+Soulcitywebdeveloper@users.noreply.github.com>
2023-07-23 09:33:21 +0100

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * @f: pointer to a function used to format the name
  *
  * This function takes a name and a formatting function as arguments.
- * The name is passed to the formatting function, which formats the name
+ * The name s passed to the formatting function, which formats the name
  * according to whatever rules it specifies. The formatted name is
  * then printed to stdout.
  */
