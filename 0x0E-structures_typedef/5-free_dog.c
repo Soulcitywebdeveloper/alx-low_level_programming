6befaf47f5f2404ad2c2987b298ba7ea8d54f003
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-07-23 01:08:59 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees a struct dog
+ * @d: pointer to struct dog to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
