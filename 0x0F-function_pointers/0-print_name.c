a7a0825ab8bc3f6e3fde0e6068fd977a3e03454f
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-07-23 03:08:11 -0700

    why my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer to format name
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: void
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
