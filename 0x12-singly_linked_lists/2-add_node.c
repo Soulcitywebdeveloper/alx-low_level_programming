9eda884e21dbfded24b0a81d5511d9417ba00051
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-07-27 16:10:11 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,22 +9,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (head == NULL || str == NULL)
+	list_t *new;
+	size_t nchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new->str = strdup(str);
 
-	if (new_node == NULL)
-	return (NULL);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	return (*head);
 }
