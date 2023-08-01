1e2fd4610c06f8174dcd1230181bba53e40134da
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-08-01 15:41:15 -0700

    g

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	while ((temp = head) != NULL)
+	while ((tmp = head) != NULL)
 	{
 		head = head->next;
-		free(temp);
+		free(tmp);
 	}
 }
