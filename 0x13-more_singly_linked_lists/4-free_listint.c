286bef1a9a0e36b31d33fd85abb5762d6d655937
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-08-01 15:00:41 -0700

    free list int

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	return;
+
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
