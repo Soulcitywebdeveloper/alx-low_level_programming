6ed08d82c7046d6ce4933d7346d8e294d2d53c68
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-08-07 15:30:57 -0700

    mcreate file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-    
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1); /* Return -1 on failure to open file */
@@ -29,12 +28,12 @@ int create_file(const char *filename, char *text_content)
 		len++;
 
 
- 	write_len = write(fd, text_content, len);
+	write_len = write(fd, text_content, len);
 	if (write_len != len)
 	{
 		close(fd);
 		return (-1);
-        }
+}
 	}
 
 	close(fd);
