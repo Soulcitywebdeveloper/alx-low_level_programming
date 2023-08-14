15c38c38d9df5f0160156985872bc717c2f00a43
Soulcitywebdeveloper <oyewolesulaimon@gmail.com>
2023-08-14 05:31:43 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,109 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include "main.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-#define BUF_SIZE 1024
-
-/**
- * main - check the code for ALX students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
-
-int main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		print_error("Usage: cp file_from file_to\n", "", 97);
-	}
-	else
-	{
-		int result = copy_file(argv[1], argv[2]);
-
-		if (result == 98)
-		{
-			print_error("Error: Can't read from file ", argv[1], result);
-		}
-		else if (result == 99)
-		{
-			print_error("Error: Can't write to ", argv[2], result);
-		}
-		else if (result == 100)
-		{
-			print_error("Error: Can't close fd ", "", result);
-		}
-	}
-
-	return (0);
-}
+#define BUFF_SIZE 1024
 
 /**
- * print_error - checks if files can be opened.
- * @msg: deplay
- * @file_name: name of the file
- * @code: read the code
- * Return: no return.
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: Always 0 (Success)
  */
-void print_error(char *msg, char *file_name, int code)
+int main(int ac, char *av[])
 {
-	dprintf(STDERR_FILENO, "%s%s\n", msg, file_name);
-	exit(code);
-}
+	int fd_from, fd_to, r, w;
+	char buffer[BUFF_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-/**
- * copy_file - checks if files can be opened.
- * @file_from: file to copy from
- * @file_to: file to copy to
- * Return: no return.
- */
-int copy_file(char *file_from, char *file_to)
-{
-	int fd_from = open(file_from, O_RDONLY);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		return (98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	int fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd_to == -1)
-	{
-		close(fd_from);
-		return (99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	char buffer[BUF_SIZE];
-	ssize_t bytes_read;
+	do {
+		r = read(fd_from, buffer, BUFF_SIZE);
+		if (r == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
-	{
-		ssize_t bytes_written = write(fd_to, buffer, bytes_read);
+		w = write(fd_to, buffer, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	} while (r == BUFF_SIZE);
 
-		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			return (99);
-		}
-	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		return (98);
-	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		return (100);
-	}
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
